5664b6e5d87851efd5399836b851d0051c8e661d
Ahmed Wafy <ahmedwafyasu@gmail.com>
2026-06-16 06:28:21 +0300

    fix: fixed secret key name

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,3 +1,3 @@
-export const baseURL = process.env.NEXT_PUBLIC_SUPABASE_BASE_URL!;
+export const baseURL = process.env.NEXT_SUPABASE_BASE_URL!;
 
-export const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_SECRET_KEY!;
+export const supabaseKey = process.env.NEXT_SUPABASE_SECRET_KEY!;
